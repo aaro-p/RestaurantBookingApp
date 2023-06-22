c7a1c6534fdf5b8ae9540e791efa7f0da356bb1c
aaro-p <pennanen.aaro@gmail.com>
2023-06-22 19:40:03 +0300

    Add more components + intsall prisma

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,21 +1,14 @@
-import Image from "next/image";
-import NavBar from "../../components/NavBar";
 import Header from "./components/Header";
 import ReservationForm from "./components/ReservationForm";
 
 const ReservePage = (): JSX.Element => {
     return (
-        <main className="bg-gray-100 min-h-screen w-screen">
-            <main className="max-w-screen-2xl m-auto bg-white">
-                <NavBar />
-                <div className="border-t h-screen">
-                    <div className="py-9 w-3/5 m-auto">
-                        <Header />
-                        <ReservationForm />
-                    </div>
-                </div>
-            </main>
-        </main>
+        <div className="border-t h-screen">
+            <div className="py-9 w-3/5 m-auto">
+                <Header />
+                <ReservationForm />
+            </div>
+        </div>
     );
 };
 
